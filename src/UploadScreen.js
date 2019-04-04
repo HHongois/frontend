930536901b50cfec1fe59655d94fd3b07e293e2e
hubert.hongois@gmail.com <hubert.hongois@gmail.com>
2019-04-04 14:25:08 +0200

    inscription ok

---
+++ src/UploadScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class UploadScreen extends Component {
+    constructor(){
+        super();
+    }
+
+    // render(){
+
+    // }
+
+}  
+  export default UploadScreen;
